5265a8fc023acefb49293a93f40e0a815baa781f
ppiotrlach <lachpio999@gmail.com>
2022-05-03 15:45:06 +0200

    Api mockup

--- lab4/groupfinder/src/App.js
+++ lab4/groupfinder/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Home from './Components/Home';
 import NoticeAdder from './Components/NoticeAdder';
 import MessageSender from './Components/MessageSender';
@@ -8,25 +8,36 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import axios from "axios";
 
 function App() {
   const [wantedGroup, setWantedGroup] = useState(null);
-  const [wantedGroups, setWantedGroups] = useState([
-    {
-      name: "Piotrek IGM",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      email: "p@gmail.com",
-      tags: "ucisw2 project tuesday",
-      classes: "UCISW 2 PROJECT tuesday"
-    },
-    {
-      name: "Mientus2000",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      email: "m@gmail.com",
-      tags: "ripo project wednesday asap",
-      classes: "RIPO - project"
-    }
-  ]);
+  const [wantedGroups, setWantedGroups] = useState([]);
+  //   {
+  //     name: "Piotrek IGM",
+  //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  //     email: "p@gmail.com",
+  //     tags: "ucisw2 project tuesday",
+  //     classes: "UCISW 2 PROJECT tuesday"
+  //   },
+  //   {
+  //     name: "Mientus2000",
+  //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  //     email: "m@gmail.com",
+  //     tags: "ripo project wednesday asap",
+  //     classes: "RIPO - project"
+  //   }
+  // ]);
+
+
+  useEffect(() => {
+    axios
+      .get("/api/groups.json")
+      .then((res) => {
+        // console.log(res.data);
+        setWantedGroups(res.data);
+      })
+  })
 
   const onNoticeAdd = (Notice) => {
     setWantedGroups(wantedGroups => [...wantedGroups, Notice]);
